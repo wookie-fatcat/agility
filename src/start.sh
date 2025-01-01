3c8c03e90d4ad52f1509272dcc5e0f54f0cc113b
Rob Tweed <rtweed@mgateway.com>
2025-01-01 14:59:56 +0000

    Added externally-run docker exec command files

--- src/start.sh
+++ src/start.sh
@@ -1,11 +1,16 @@
 # Script run whenever mg-showcase Container is started
 
+dos2unix /opt/mgateway/mapped/*
 chmod +x /opt/mgateway/mapped/tail
 chmod +x /opt/mgateway/mapped/start
 chmod +x /opt/mgateway/mapped/stop
 chmod +x /opt/mgateway/mapped/restart
 chmod +x /opt/mgateway/mapped/log
 chmod +x /opt/mgateway/mapped/clearLog
+chmod +x /opt/mgateway/mapped/agility_start
+chmod +x /opt/mgateway/mapped/agility_stop
+chmod +x /opt/mgateway/mapped/agility_restart
+chmod +x /opt/mgateway/mapped/agility_log
 
 echo "Permissions set for commands in mapped directory"
 
