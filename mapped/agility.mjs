74e35514b9b361b33dc9ef85a24666d8c9fe9599
wookie-fatcat <nick@pinpoint.co.uk>
2025-02-27 12:46:45 +0000

    Update agility.mjs

--- mapped/agility.mjs
+++ mapped/agility.mjs
@@ -542,7 +542,8 @@ let Agility = class {
 
   async getLatestVersionNo() {
     //let url = 'https://cdn.jsdelivr.net/gh/robtweed/agility/mapped/version.json';
-    let url = 'https://raw.githubusercontent.com/robtweed/agility/refs/heads/master/mapped/version.json';
+    //let url = 'https://raw.githubusercontent.com/robtweed/agility/refs/heads/master/mapped/version.json';
+    let url = 'https://raw.githubusercontent.com/wookie-fatcat/agility/refs/heads/master/mapped/version.json';
     let options = {
       method: 'GET',
       headers: {
@@ -616,7 +617,8 @@ let Agility = class {
       }); 
     }
     fs.mkdirSync(copyDir);
-    this.exec('git clone https://github.com/robtweed/agility ' + copyDir);
+    //this.exec('git clone https://github.com/robtweed/agility ' + copyDir);
+    this.exec('git clone https://github.com/wookie-fatcat/agility ' + copyDir);
     let fileArr = fs.readdirSync(copyDir + '/mapped');
     for (let filename of fileArr) {
       if (filename.endsWith('.mjs') || filename === 'version.json') {
@@ -668,4 +670,3 @@ let Agility = class {
 }
 
 export {Agility}
-
